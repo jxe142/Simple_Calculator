5b8b0d42255966a8c7384c5d5b661b323ecbb46a
Joel Evans <joelevans@Joels-MBP.wssu.edu>
2018-02-12 11:39:29 -0500

    3rd commit

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
 
 application = Flask(__name__)
-app = application
-app.debug = True
-app.secret_key = 'cC1YCsdOjQQWEGgWsApgNEo2'
+application.debug = True
+application.secret_key = 'cC1YCsdOjQQWEGgWsApgNEo2'
 
 
-@app.route("/", methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@application.route("/", methods=['GET', 'POST'])
+@application.route('/index', methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    application.run()
